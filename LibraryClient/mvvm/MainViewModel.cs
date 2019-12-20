ac1bd764f6f80c22ca9cf7d8bd03a2675a44f7b4
woipot <frostik0409@gmail.com>
2019-12-21 00:56:51 +0300

    user add posibility

--- LibraryClient/mvvm/MainViewModel.cs
+++ LibraryClient/mvvm/MainViewModel.cs
@@ -9,6 +9,9 @@ namespace LibraryClient.mvvm
 {
     public class MainViewModel : BindableBase
     {
+        public User ForAddUser { get; } = new User();
+
+        public DelegateCommand AddUserCommand { get; }
 
         public List<User> Users { get; private set; }
         public List<Book> Books { get; private set; }
@@ -22,7 +25,18 @@ namespace LibraryClient.mvvm
                 Users = db.User.ToList();
                 Books = db.Book.ToList();
                 Orders = db.Orders.ToList();
-                
+            }
+
+            AddUserCommand = new DelegateCommand(AddUser);
+        }
+
+        private void AddUser()
+        {
+            using (mydbContext db = new mydbContext())
+            {
+                db.User.Add(ForAddUser);
+                Users.Add(ForAddUser);
+               
             }
         }
     }
